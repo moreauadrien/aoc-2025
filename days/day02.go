d128e165e5975fbe76b47055d339771f69573f0f
Adrien Moreau <moreauadrien92@gmail.com>
2026-02-01 17:45:37 +0100

    day2

--- days/day02.go
+++ days/day02.go
@@ -1,67 +1,89 @@
 package days
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
+type idRange struct {
+	start int
+	end   int
+}
+
 func (d Days) Day02(input string) (string, string) {
-	moves := parse(input)
-	sol_part1 := part1(moves)
-	sol_part2 := part2(moves)
+	ranges := day02_parse(input)
+	sol_part1 := day02(ranges, isValidId_part1)
+	sol_part2 := day02(ranges, isValidId_part2)
 
 	return strconv.Itoa(sol_part1), strconv.Itoa(sol_part2)
 }
 
-func parse(input string) []int64 {
-	lines := strings.Split(input, "\n")
-	moves := make([]int64, 0, len(lines))
+func day02_parse(input string) []idRange {
+	str_ranges := strings.Split(input, ",")
+	ranges := make([]idRange, 0, len(str_ranges))
 
-	for _, l := range lines {
-		left := l[0] == 'L'
-		val, err := strconv.ParseInt(l[1:], 10, 0)
-		if err != nil {
-			panic(err)
+	for _, r := range str_ranges {
+		parts := strings.Split(r, "-")
+
+		s, err_s := strconv.Atoi(parts[0])
+		e, err_e := strconv.Atoi(parts[1])
+
+		if err_s != nil || err_e != nil {
+			panic(fmt.Sprintf("Can't parse the range '%v'", r))
 		}
 
-		if left {
-			val = -val
-		}
-
-		moves = append(moves, val)
+		ranges = append(ranges, idRange{s, e})
 	}
 
-	return moves
+	return ranges
 }
 
-func part1(moves []int64) int {
-	var dial int64 = 50
-	count := 0
+func isValidId_part1(n int) bool {
+	n_str := strconv.Itoa(n)
+	l := len(n_str)
 
-	for _, m := range moves {
-		dial = (dial + m) % 100
-		if dial == 0 {
-			count += 1
-		}
+	if l%2 != 0 {
+		return true
 	}
 
-	return count
+	return n_str[0:l/2] != n_str[l/2:l]
 }
 
-func part2(moves []int64) int {
-	var dial int64 = 50
+func day02(ranges []idRange, isValidId func(int) bool) int {
 	count := 0
-	for _, m := range moves {
-		var step int64 = 1
-		if m < 0 {
-			step = -1
-		}
-		for i := int64(0); i != m; i += step {
-			dial = ((dial+step)%100 + 100) % 100
-			if dial == 0 {
-				count++
+
+	for _, r := range ranges {
+		for i := r.start; i <= r.end; i += 1 {
+			if isValidId(i) == false {
+				count += i
 			}
 		}
 	}
+
 	return count
+}
+
+func isValidId_part2(n int) bool {
+	n_str := strconv.Itoa(n)
+	l := len(n_str)
+
+outer:
+	for win_size := 1; win_size <= l/2; win_size += 1 {
+		if l%win_size != 0 {
+			continue
+		}
+
+		patt := n_str[0:win_size]
+
+		for w := win_size; w+win_size <= l; w += win_size {
+			if n_str[w:w+win_size] != patt {
+				continue outer
+			}
+		}
+
+		return false
+	}
+
+	return true
 }
