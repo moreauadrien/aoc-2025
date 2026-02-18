befdb3c93f3dc7155614e24fe6b8f077275b1995
Adrien Moreau <moreauadrien92@gmail.com>
2026-02-18 15:20:13 +0100

    working day09

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math"
+
 func Check(err error) {
 	if err != nil {
 		panic(err)
@@ -10,4 +12,36 @@ func Assert(b bool, msg string) {
 	if b == false {
 		panic(msg)
 	}
+}
+
+func IntAbs(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
+}
+
+func IntMax(n ...int) int {
+	m := math.MinInt
+
+	for _, e := range n {
+		if e > m {
+			m = e
+		}
+	}
+
+	return m
+}
+
+func IntMin(n ...int) int {
+	m := math.MaxInt
+
+	for _, e := range n {
+		if e < m {
+			m = e
+		}
+	}
+
+	return m
 }
